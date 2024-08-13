04e84f1076be3d8b47acfa1eaaca51ff53f42eb3
Teplov Max <maksim.teplov@vitech.team>
2024-08-13 08:30:03 +0700

    fix comment

--- internal/builder/grpc.go
+++ internal/builder/grpc.go
@@ -1,7 +1,7 @@
 package builder
 
 import (
-	grpc2 "github.com/tweeker88/chat-server-oleg/internal/transport/grpc"
+	grpc_gen "github.com/tweeker88/chat-server-oleg/internal/transport/grpc"
 	"github.com/tweeker88/chat-server-oleg/pkg/chat_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -11,7 +11,7 @@ func BuildServer() *grpc.Server {
 	srv := grpc.NewServer()
 	reflection.Register(srv)
 
-	my := grpc2.NewServer()
+	my := grpc_gen.NewServer()
 
 	chat_v1.RegisterChatV1Server(srv, my)
 
